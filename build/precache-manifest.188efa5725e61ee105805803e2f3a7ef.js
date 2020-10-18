6187ade9ff697cb4d77b19d1ce1b4151b0bd48fc
Chaitanya0003 <tiwadic@gmail.com>
2020-10-18 20:21:18 +0530

    c

---
+++ build/precache-manifest.188efa5725e61ee105805803e2f3a7ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22ca9f132f002c812a625b95d4a32576",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff08b23db5c29a3304eb",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "a380150f363b917fbc91",
+    "url": "/static/css/main.5905525a.chunk.css"
+  },
+  {
+    "revision": "ff08b23db5c29a3304eb",
+    "url": "/static/js/2.0340ae21.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0340ae21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a380150f363b917fbc91",
+    "url": "/static/js/main.7f677835.chunk.js"
+  },
+  {
+    "revision": "af1e4e28e43c946b2ef8",
+    "url": "/static/js/runtime-main.d0b8447a.js"
+  }
+]);
